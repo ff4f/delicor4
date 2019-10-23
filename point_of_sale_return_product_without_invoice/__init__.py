c0f9bc28d437f05e1e383a4669bcffdedc218c29
franklin justiniano <frxnco1802@gmail.com>
2019-10-23 12:01:54 -0400

    eliminacion addons innecesarios

--- point_of_sale_return_product_without_invoice/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: utf-8 -*-
-#
-# Module written to Odoo, Open Source Management Solution
-
-# Developer(s): Luis Ernesto García Medina
-#               (ernesto.r.2.em@gmail.com)
-########################################################################
-
-from . import models
