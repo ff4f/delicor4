5fa1d4d1e39113739ca54f0b863b119cfd06091a
franklin justiniano <frxnco1802@gmail.com>
2019-11-04 12:17:09 -0400

    prueba

--- l10n_bo_point_of_sale/models/account_invoice.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models, api, _
-
-
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
-
-    amount_change = fields.Float('Cambio')
