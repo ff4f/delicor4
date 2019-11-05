13110234c651ca9321a6113dc95d78adcac9ec31
franklin justiniano <frxnco1802@gmail.com>
2019-11-05 11:37:53 -0400

    prueba

--- l10n_bo_account_invoice/l10n_bo_account_invoice/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import account_tax
-from . import account_invoice
-from . import account_account
-from . import sale
-from . import company
-__all__ = ["numero_a_letra"]
-
